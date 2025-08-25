ee73b279735d1e06df49399a51b64043925ee1e4
Nam <toymyorange@gmail.com>
2025-08-25 15:40:52 +0700

    add cards animation on players' hand

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,42 +1,20 @@
 extends Area2D
 
+signal hovered(card: Area2D)
+signal unhovered(card: Area2D)
+
 @export var card_color: String
 @export var card_value: String
 
+var tween: Tween
 var original_position: Vector2
-var done = false
+var setup_finished = false
 
 func _ready():
 	original_position = position
-	if done:
-		update_card_texture()
-
-	# Connect signals if not done in the editor
-	connect("mouse_entered", Callable(self, "_on_mouse_entered"))
-	connect("mouse_exited", Callable(self, "_on_mouse_exited"))
-
-func _input(event: InputEvent) -> void:
-	if event == InputEventMouseButton:
-		var mouse_over = get_global_mouse_position().distance_to(global_position) < 150
-		if mouse_over:
-			hover_up()
-		else:
-			hover_down()
-			
-func hover_up():
-	if position.y == original_position.y-20:
-		return
-	var tween = create_tween()
-	tween.tween_property(self, "position", original_position + Vector2(0, -20), 0.2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
-
-func hover_down():
-	if position.y == original_position.y:
-		return
-	var tween = create_tween()
-	tween.tween_property(self, "position", original_position, 0.2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 
 func setup(color: String, value: String):
-	done = true
+	setup_finished = true
 	card_color = color
 	card_value = value
 	
@@ -49,11 +27,29 @@ func update_card_texture():
 	else:
 		push_warning("Card texture not found: %s %s" % [card_color, card_value])
 
-func _on_mouse_entered():
+func update_original_pos(pos):
+	original_position = pos
+
+func hover_up():
 	if position.y == original_position.y-20:
 		return
 	var tween = create_tween()
-	tween.tween_property(self, "position", original_position + Vector2(0, -20), 0.2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "position:y", original_position.y-20, 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
 
-func _on_mouse_exited():
-	position = original_position
+func hover_down():
+	if position.y == original_position.y:
+		return
+	var tween = create_tween()
+	tween.tween_property(self, "position:y", original_position.y, 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+
+func get_rect() -> Rect2:
+	if $Sprite2D.texture:
+		var tex_size = $Sprite2D.texture.get_size()
+		return Rect2(-tex_size.x / 2, -tex_size.y / 2, tex_size.x, tex_size.y)
+	return Rect2()
+
+func _on_mouse_entered() -> void:
+	hovered.emit(self)
+
+func _on_mouse_exited() -> void:
+	unhovered.emit(self)
--- scripts/hand.gd
+++ scripts/hand.gd
@@ -3,6 +3,25 @@ extends Node2D
 var CardScene = preload("res://scenes/Card.tscn")
 var owned = false
 
+func _process(delta: float):
+	var mouse_pos = get_global_mouse_position()
+	var card_to_be_raised_idx = -1
+	var z_max = -999
+	
+	for i in get_child_count():
+		var card = get_child(i)
+		if card.get_rect().has_point(card.to_local(mouse_pos)):
+			if card.z_index > z_max:
+				z_max = card.z_index
+				card_to_be_raised_idx = i
+				
+	for i in get_child_count():
+		var card = get_child(i)
+		if i == card_to_be_raised_idx:
+			card.hover_up()
+		else:
+			card.hover_down()
+
 func spawn_card_with_slide(color, value):
 	var card = CardScene.instantiate()
 	card.setup(color, value)
@@ -34,3 +53,4 @@ func layout_hand():
 		# tween for a smooth slide
 		var tween = create_tween()
 		tween.tween_property(card, "position", target_pos, 0.3).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+		card.update_original_pos(card.position)
